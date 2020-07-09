1b48a5df7a660d3dd1e0c21a85fdb95a1ee5922b
Eel2000 <eliezerbwana@gmaill.com>
2020-07-09 13:26:26 +0200

    adding splash screen and toast msg

---
+++ SOS_Sante/ActivitySplash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SOS_Sante
+{
+    [Activity(Label = "ActivitySplash",MainLauncher =true, Theme = "@style/SplashTheme",NoHistory =true)]
+    public class ActivitySplash : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Thread.Sleep(2000);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
--- SOS_Sante/MainActivity.cs
+++ SOS_Sante/MainActivity.cs
@@ -3,10 +3,11 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Support.Design.Widget;
 
 namespace SOS_Sante
 {
-    [Activity(Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Theme = "@style/AppTheme", MainLauncher = false)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -15,7 +16,17 @@ namespace SOS_Sante
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.FabCall);
+
+            fab.Click += Fab_Click;
         }
+
+        private void Fab_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "You.ve call Nearly Emergency Ambulance", ToastLength.Long).Show();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
